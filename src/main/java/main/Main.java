d29f030a4ce618f660f74403aef30529aa988361
messele ghebreslassie <messeleghebreslassie@gmail.com>
2022-09-11 20:07:49 -0600

    initial setup

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,2 +1,17 @@
-package main;public class Main {
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame j = new JFrame();
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setResizable(false);
+        j.setTitle("2D Adventure Game");
+
+        j.setLocationRelativeTo(null);
+        j.setVisible(true);
+    }
 }
